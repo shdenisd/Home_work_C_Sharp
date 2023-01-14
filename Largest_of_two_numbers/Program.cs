65664c2ca84e33496b1eb86bbb8b5f52f27f8cd2
denis <hugostiglitz590@gmail.com>
2023-01-14 12:20:05 +0300

    First HomeWork done

---
+++ Largest_of_two_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Дорогой друг, введи, пожалуйста, два числа, а я определю их размерность: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Теперь второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA > numberB)
+{
+    Console.Write($"Число {numberA} является наибольшим, а число {numberB} является наименьшим");
+}
+else if (numberA < numberB)
+{
+    Console.Write($"Число {numberB} является наибольшим, а число {numberA} является наименьшим");
+}
+else
+{
+    Console.Write($"Числа равны");
+}
+
+
+
