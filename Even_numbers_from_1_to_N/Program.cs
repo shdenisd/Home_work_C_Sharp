35a7dbbe6062d3994839339e7aeba4d0010f1472
denis <hugostiglitz590@gmail.com>
2023-01-14 13:48:28 +0300

    Fourth HomeWork done

---
+++ Even_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите натуральное число, программа покажет все чётные числа от 1 до вашего числа: ");
+int UserNumber = int.Parse(Console.ReadLine()!);
+
+if (UserNumber < 2)
+{
+    Console.WriteLine("Таких чисел нет :(");
+}
+
+int counter = 2;
+while (counter <= UserNumber)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write($"{counter} ");
+    }
+    counter++;
+}
+
