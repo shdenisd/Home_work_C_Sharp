b02b26396bc931110116c4e107b4b7531674acfa
denis <hugostiglitz590@gmail.com>
2023-01-14 13:27:19 +0300

    Third HomeWork done

---
+++ even_or_not_even/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Привет! Введи число, а я скажу чётное оно или нет: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Твоё число чётное!");
+}
+else
+{
+    Console.WriteLine("Твоё число нечётно!");
+}
+
