99c6bcad8175c05dd6c3c4a8616221c2e4fd556a
denis <hugostiglitz590@gmail.com>
2023-01-14 12:45:58 +0300

    Second HomeWork done

---
+++ Largest_of_three_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите ниже 3 числа подряд, а я узнаю наибольшее:");
+
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = int.Parse(Console.ReadLine()!);
+int numberC = int.Parse(Console.ReadLine()!);
+
+
+if (numberA >= numberB && numberA >= numberC)
+{
+    Console.WriteLine($"Наибольшим числом является {numberA}");
+}
+else if (numberB >= numberA && numberB >= numberC)
+{
+    Console.WriteLine($"Наибольшим числом является {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Наибольшим числом является {numberC}");
+}
