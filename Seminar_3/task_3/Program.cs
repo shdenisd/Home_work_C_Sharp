cd2079a4126734585353121b6f06b38e2845b72a
denis <hugostiglitz590@gmail.com>
2023-01-22 17:23:40 +0300

    Added new home work - Seminar 3, task 3

---
+++ Seminar_3/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+
+System.Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+
+System.Console.Write($"Кубы чисел от 1 до {number}:  ");
+
+int counter = 1;
+while (counter <= number)
+{
+    System.Console.Write(Math.Pow(counter, 3));
+    if (counter != number) System.Console.Write(", ");
+    counter++; 
+}
