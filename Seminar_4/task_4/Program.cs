a28254e13da94d1e76c8e5c194b03e8948fba4ee
denis <hugostiglitz590@gmail.com>
2023-01-26 19:14:22 +0300

    added new homework - seminar_4, task 4

---
+++ Seminar_4/task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите функцию, которая
+// принимает одно число - высоту елочки и рисует ее в
+// консоли звездочками.
+// 3 ->  
+//  * 
+// ***
+//*****
+// 5 ->  
+//       *
+//      ***
+//     *****
+//    *******
+//   *********
+
+void print_fir (int number)
+{
+    for (int i = 1; i <= number; i++)
+    {   
+        System.Console.WriteLine(" ");
+        for (int j = 0; j < number + i; j++)
+        {
+            if (j > number - i) System.Console.Write("*");
+            else System.Console.Write(" ");
+        }
+    }
+}
+
+System.Console.WriteLine("Введите высоту ёлочки");
+
+int number = 5;
+//int number = int.Parse(Console.ReadLine()!);
+
+print_fir(number);
