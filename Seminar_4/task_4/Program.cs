898cc1762fcda8ee39018dd948eec5c73deeeb2e
Denis <hugostiglitz590@gmail.com>
2023-01-30 09:00:56 +0300

    created new homework- seminar 5, task 1

--- Seminar_4/task_4/Program.cs
+++ Seminar_4/task_4/Program.cs
@@ -6,28 +6,30 @@
 // ***
 //*****
 // 5 ->  
-//       *
-//      ***
-//     *****
-//    *******
-//   *********
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  
+//  *
+// ***
 
 void print_fir (int number)
 {
     for (int i = 1; i <= number; i++)
     {   
-        System.Console.WriteLine(" ");
         for (int j = 0; j < number + i; j++)
         {
             if (j > number - i) System.Console.Write("*");
             else System.Console.Write(" ");
         }
+        System.Console.WriteLine(" ");
     }
 }
 
 System.Console.WriteLine("Введите высоту ёлочки");
 
-int number = 5;
-//int number = int.Parse(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 
 print_fir(number);
---
+++ Seminar_5/task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// **Задача 34**: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] get_random_array(int size, int minValue, int maxValue)
+{
+    int[] random_array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        random_array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return random_array;
+}
+
+
+void count_even_numbers(int[] array)
+{
+    int counter = 0;
+    foreach(int el in array)
+    {
+        if(el % 2 == 0) counter++;
+    }
+    System.Console.WriteLine($"Колличество чётных, трёхзнычных элементов в массиве = {counter}");
+}
+
+
+
+int[] array = get_random_array(5, 100, 999);
+System.Console.WriteLine(string.Join(" ", array));
+
+count_even_numbers(array);
